424f689bf130de2787ed0c5fab89a5b8979621b7
Ada-lave <mega.kefi36@gmail.com>
2026-05-04 13:03:20 +0500

    1071

--- go/1071/1071.go
+++ go/1071/1071.go
@@ -1,46 +1,16 @@
 package task_1071
 
-import "strings"
-
-func FindAllSubStrings(text string) []string {
-	subStringMap := []string{}
-	for i := 0; i < len(text) - 1; i++ {
-		for j := i + 1; j < len(text); j++ {
-			subStringMap = append(subStringMap, text[i:j + 1])
-		
-		}
-	}
-
-	if len(text) == 1 {
-		subStringMap = append(subStringMap, text)
-	}
-
-	return subStringMap
-}
-
-func FindSubStrCount(text string, sub string) int {
-	count := 1
-	for i := 0; i < len(text) - 1; i++ {
-		for j := i + 1; j < len(text); j++ {
-			if text[i:j + 1] == sub {
-				count++
-			}
-		
-		}
-	}
-	return count
-}
-
 func gcdOfStrings(str1 string, str2 string) string {
-    
-	subMap1 := FindAllSubStrings(str2)
-
-	for _, v := range subMap1 {
-		subCount := FindSubStrCount(str1, v)
-		if strings.Repeat(v, subCount) == str1 {
-			return v
-		}
+	if str1+str2 != str2+str1 {
+		return ""
 	}
+	n := gcd(len(str1), len(str2))
+	return str1[:n]
+}
 
-	return ""
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
--- go/1071/1071_test.go
+++ go/1071/1071_test.go
@@ -38,11 +38,47 @@ func Test_gcdOfStrings(t *testing.T) {
 			str2: "A",
 			want: "A",			
 		},
+		struct{name string; str1 string; str2 string; want string}{
+			name: "case 5",
+			str1: "ABABABAB",
+			str2: "ABAB",
+			want: "ABAB",			
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := gcdOfStrings(tt.str1, tt.str2)
 			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_gcd(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		a    int
+		b    int
+		want int
+	}{
+		struct{name string; a int; b int; want int}{
+			name: "case 1",
+			a: 18,
+			b: 12,
+			want: 6,
+		},
+		struct{name string; a int; b int; want int}{
+			name: "case 2",
+			a: 12,
+			b: 18,
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := gcd(tt.a, tt.b)
+			// TODO: update the condition below to compare got with tt.want.
+			require.Equal(t, got, tt.want)
 		})
 	}
 }
