f85167a570982ca037f901434ce81d2baaa527f5
Ada-lave <mega.kefi36@gmail.com>
2026-06-18 13:04:22 +0500

    p1679 rust way

--- rust/src/problems/p1679/solution.rs
+++ rust/src/problems/p1679/solution.rs
@@ -3,19 +3,25 @@ pub struct Solution;
 impl Solution {
     pub fn max_operations(mut nums: Vec<i32>, k: i32) -> i32 {
         let mut counter = 0;
-        nums.sort();
-        let (mut idx1, mut idx2) = (0 as usize, nums.len() - 1);
+        nums.sort_unstable();
+        let (mut idx1, mut idx2) = (0, nums.len() - 1);
 
         while idx1 < idx2 {
             let sum_of_idxs = nums[idx1] + nums[idx2];
-            if  sum_of_idxs == k {
-                counter += 1;
-                idx1 += 1;
-                idx2 -= 1
-            }   else if sum_of_idxs < k {
-                idx1 += 1
-            } else {
-                idx2 -= 1
+            match sum_of_idxs.cmp(&k) {
+                std::cmp::Ordering::Equal => {
+                    counter += 1;
+                    idx1 += 1;
+                    idx2 -= 1
+                }
+                
+                std::cmp::Ordering::Greater => {
+                    idx2 -= 1
+                }
+
+                std::cmp::Ordering::Less => {
+                    idx1 += 1
+                }
             }
         }
 
